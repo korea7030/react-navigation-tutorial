85553a6c0b5de4e3e9fea738d4be76dc86fe6a49
korea7030 <korea7030@naver.com>
2020-11-19 12:33:42 +0900

    Nesting Navigators

--- App.js
+++ App.js
@@ -3,98 +3,67 @@ import { Button, View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
-  const [count, setCount] = React.useState(0);
-
-  React.useLayoutEffect(() => {
-    // screen component의 state를 사용하기 위해서는
-    // navigation.setOptions를 사용해서 접근 가능
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount(c => c + 1)} title="Update Count" />
-      ),
-    });
-  }, [navigation]);
-
+function SettingsScreen({ route, navigation }) {
+  const { user } = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Update the title"
-       // update option use setOptions
-        onPress={() => navigation.setOptions({title: 'Updated!'})} />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+      <Text>userParam: {JSON.stringify(user)}</Text>
       <Button
         title="Go to Profile"
-        onPress={() => navigation.navigate('Profile', { name: 'Custom profile header' })}/>
-      <Text>Count: {count}</Text>
+        onPress={() => navigation.navigate('Profile')}
+      />
     </View>
-  )
+  );
 }
 
 function ProfileScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Profile Screen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()}/>
     </View>
   );
 }
 
-function LogoTitle() {
+function HomeScreen({ navigation }) {
   return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={require('@expo/snack-static/react-native-logo.png')}
-    />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() =>
+        // Root의 Settings screen으로 params 을 전달
+        navigation.navigate('Root', {
+          screen: 'Settings',
+          params: { user: 'jane'},
+        })
+      }
+      />
+    </View>
   );
 }
 
-// headerBackTitle and headerTruncatedBackTitle : 기존에 제공하는 back button을 변경하기 위해서 사용하는 component
+// 왼쪽 드래그 navigator
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
+function Root() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        // 여러 스크린에 스타일 적용할 때 사용
-        screenOntions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: 'My home',
-            // 해당 screen 에서만 적용됨. screen이 넘어가면 원래 style로 돌아감
-            // headerStyle: {
-            //   backgroundColor: '#f4511e',
-            // },
-            // headerTintColor: '#fff',
-            // header title custom component
-            // (headerTitle: 스택 탐색기에 특정된 속성이며 제목을 표시하는 텍스트 구성 요소로 기본 설정되어 있기 때문에 headerTitle에 설정해야 함)
-            headerTitle: props => <LogoTitle {...props} />,
-            // 이렇게 버튼을 지정할 경우,
-            // this 변수가 options의 값을 가리키게 됨. setState또는 instance내의 methods를 호출하지 못함
-            headerRight: () => (
-              <Button
-                onPress={() => alert('This is a button!')}
-                title="Info"
-                color="#00cc00"
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="profile"
-          component={ProfileScreen}
-          options={({ route }) => ({ title: route.params.name })} />
-      </Stack.Navigator>
+      {/* 왼쪽 drawer navigator  */}
+      <Drawer.Navigator initialRouteName="Root">
+        <Drawer.Screen name="Root" component={Root} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
