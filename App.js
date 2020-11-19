8572e2cd9ca27df8ab51aed8d4586c605015232f
korea7030 <korea7030@naver.com>
2020-11-19 13:35:32 +0900

    Opening a full-screen modal

--- App.js
+++ App.js
@@ -1,115 +1,57 @@
 import * as React from 'react';
-import { Button, View, Text, Image } from 'react-native';
-import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function SettingsScreen({ route, navigation }) {
-  const { user } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-      <Text>userParam: {JSON.stringify(user)}</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  );
-}
-
-function ProfileScreen({ navigation }) {
-  // useIsFocused : 화면의 focus 상태에 따라 다른 content rendering을 하는 방법으로 제공하는 hook
-
-  // screen이 focus 됐을 때 listen
-  // React.useEffect(
-  //   () => navigation.addListener('focus', () => alert('Screen was focused')),
-  //   []
-  // );
-
-  // screen이 focus가 떠났을 때 listen
-  // React.useEffect(
-  //   () => navigation.addListener('blur', () => alert('Screen was unfocused')),
-  //   []
-  // );
-  // react native event listener => React.useEffect 와 같은 역할
-  useFocusEffect(
-    React.useCallback(() => {
-      alert('Screen was focused');
-      // Do something when the screen is focused
-      return () => {
-        alert('Screen was unfocused');
-        // Do something when the screen is unfocused
-        // Useful for cleanup functions
-      };
-    }, [])
-  );
-
-  return (
-    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-}
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text style={{ fontSize: 30 }}>This is the home screen!</Text>
       <Button
-        title="Go to Settings"
-        onPress={() =>
-        // Root의 Settings screen으로 params 을 전달
-        navigation.navigate('Root', {
-          screen: 'Settings',
-          params: { user: 'jane'},
-        })
-      }
+        onPress={() => navigation.navigate('MyModal')}
+        title="Open Modal"
       />
     </View>
   );
 }
 
-function DetailsScreen({ navigation }) {
+function ModalScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+      <Button onPress={() => navigation.goBack()} title="Dismiss" />
     </View>
   );
 }
 
-const Tab = createBottomTabNavigator();
-const SettingsStack = createStackNavigator();
-const HomeStack = createStackNavigator();
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
+
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
+
+function MainStackScreen() {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={HomeScreen} />
+      <MainStack.Screen name="Details" component={DetailsScreen} />
+    </MainStack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="First">
-          {() => (
-          <SettingsStack.Navigator>
-          <SettingsStack.Screen
-            name="Settings"
-            component={SettingsScreen}
-          />
-          <SettingsStack.Screen name="Profile" component={ProfileScreen} />
-        </SettingsStack.Navigator>
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="Second">
-          {() => (
-            <HomeStack.Navigator>
-              <HomeStack.Screen name="Home" component={HomeScreen} />
-              <HomeStack.Screen name="Details" component={DetailsScreen} />
-            </HomeStack.Navigator>
-          )}
-        </Tab.Screen>
-      </Tab.Navigator>
+      {/* modal : ios에서는 작동하지만, Android는 작동안함(전체 화면 모달 전환 동작이 없기 때문) */}
+      <RootStack.Navigator mode="modal" headerMode="none">
+        <RootStack.Screen name="Main" component={MainStackScreen} />
+        <RootStack.Screen name="MyModal" component={ModalScreen} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
