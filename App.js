03fc9bfea232b3da22d36a0a94b7e8f0f963bb4a
korea7030 <korea7030@naver.com>
2020-11-20 13:58:53 +0900

    Authentication flows

--- App.js
+++ App.js
@@ -1,101 +1,142 @@
 import * as React from 'react';
-import { View, Button } from 'react-native';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-  useIsDrawerOpen,
-} from '@react-navigation/drawer';
+import { AsyncStorage, Button, Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// drawer의 open 여부를 확인
-const isDrawerOpen = useIsDrawerOpen();
+const AuthContext = React.createContext();
 
-function Feed({ navigation }) {
+function SplashScreen() {
   return (
-    <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
-      {/* <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
-      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} /> */}
-      <Button title="Open drawer" onPress={() => navigation.dispatch(DrawerActions.openDrawer())} />
-      <Button title="Toggle drawer" onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />
+    <View>
+      <Text>Loading...</Text>
     </View>
   );
 }
 
-function Notifications() {
+function HomeScreen() {
+  const { signOut } = React.useContext(AuthContext);
+
   return (
-    <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Notifications Screen</Text>
+    <View>
+      <Text>Signed in!</Text>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
 }
 
-function CustomDrawerContent(props) {
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      {/* <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.closeDrawer()}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.toggleDrawer()}
-      /> */}
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
-      />
-    </DrawerContentScrollView>
-  );
-}
+function SignInScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-function MyDrawer() {
-  return (
-    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Notifications" component={Notifications} />
-    </Drawer.Navigator>
-  );
-}
+  const { signIn } = React.useContext(AuthContext);
 
-function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
+    <View>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
       />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign in" onPress={() => signIn({ username, password })} />
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
+const Stack = createStackNavigator();
+
+function App({ naviation }) {
+  // Redux Reducer
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null
+    }
   );
-}
 
-const Drawer = createDrawerNavigator();
+  React.useEffect(() => {
+    // fetch token
+    const bootstrapAsync = async () => {
+      let userToken;
 
-function App() {
-  return (
-    <NavigationContainer>
-      {/* <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator> */}
-      <MyDrawer />
-    </NavigationContainer>
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
   );
-}
 
-export default App;
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* loading 중 */}
+          {state.isLoading? (
+            <Stack.Screen name="Splash" component={SplashScreen} />
+          ) : state.userToken == null ? (
+            // token이 없는 경우
+            <Stack.Screen
+              name="SignIn"
+              component={SignInScreen}
+              options={{
+                title: 'Sign in',
+                animationTypeForReplace: state.isSignout ? 'pop': 'push',
+              }}
+            />) : (
+              // token이 있는 경우
+              <Stack.Screen name="Home" component={Homescreen} />
+            ) }
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
+  )
+}
