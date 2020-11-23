3aa84ffdbcf723323ecfa3db5020df99a75a0e94
korea7030 <korea7030@naver.com>
2020-11-23 12:24:02 +0900

    Screen options with nested navigators

--- App.js
+++ App.js
@@ -1,61 +1,116 @@
 import * as React from 'react';
-import { Text, StatusBar, Button, StyleSheet } from 'react-native';
-import { NavigationContainer, useIsFocused } from '@react-navigation/native';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import SafeAreaView from 'react-native-safe-area-view';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
-// tab이나 drawer를 쓸 경우, 화면이 바꼈을때의 포커스 여부를 통해 화면을 표시 가능
-function FocusAwareStatusBar(props) {
-  const isFocused = useIsFocused();
+const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
 
-  return isFocused ? <StatusBar {...props} /> : null;
+// 탭의 header title을 일괄적으로 변경
+function getHeaderTitle(route) {
+  // If the focused route is not found, we need to assume it's the initial screen
+  // This can happen during if there hasn't been any navigation inside the screen
+  // In our case, it's "Feed" as that's the first screen inside the navigator
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Feed';
+
+  switch (routeName) {
+    case 'Feed':
+      return 'News feed';
+    case 'Profile':
+      return 'My profile';
+    case 'Account':
+      return 'My account';
+  }
 }
 
-function Screen1({ navigation }) {
+// A와 B는 탭 탐색기가 아니라
+// 각 Screen Stack 상의 화면 구성요소이기 때문에 options이 적용 안됨
+function A() {
+  return <View />;
+}
+
+function B() {
+  return <View />;
+}
+
+function HomeStackScreen() {
   return (
-    <SafeAreaView style={[styles.ccontainer, { backgroundColor: '#6a51ae'}]}>
-      <FocusAwareStatusBar  barStyle="light-content" backgroundColor="#6a51ae" />
-      <Text style={{ color: '#fff' }}>Light Screen</Text>
-      <Button
-        title="Next screen"
-        onPress={() => navigation.navigate('Screen2')}
-        color='#fff'
-      />
-    </SafeAreaView>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="A" component={A} />
+      {/* 아래와 같이 tabBarlabel을 지정하면 적용이 안됨 */}
+      {/* <HomeStack.Screen name="A" component={A} options={{ tabBarLabel: 'Home!' }}/> */}
+    </HomeStack.Navigator>
   );
 }
 
-function Screen2({ navigation }) {
+function SettingsStackScreen() {
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: '#ecf0f1'}]}>
-      <FocusAwareStatusBar  barStyle="dark-content" backgroundColor="#ecf0f1" />
-      <Text>Dark Screen</Text>
-      <Button
-        title="Next screen"
-        onPress={() => navigation.navigate('Screen1')}
-      />
-    </SafeAreaView>
-  )
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="B" component={B} />
+    </SettingsStack.Navigator>
+  );
 }
 
-// const Stack = createStackNavigator();
+function FeedScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return <View />;
+}
+
+function AccountScreen() {
+  return <View />;
+}
+
+function SettingsScreen() {
+  return <View />;
+}
+
 const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={FeedScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator headerMode="none">
-          <Tab.Screen name="Screen1" component={Screen1} />
-          <Tab.Screen name="Screen2" component={Screen2} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      {/* tabbar의 label 변경하려면 Tab.Screen 에서 options으로 지정해야 함 */}
+      {/* <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeStackScreen}
+          options={{ tabBarLabel: 'Home!' }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsStackScreen}
+          options={{ tabBarLabel: 'Settings!' }}
+        />
+      </Tab.Navigator> */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeTabs} options={({ route }) => ({
+    headerTitle: getHeaderTitle(route),
+  })}/>
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex:1, justifyContent: 'center', alignItems: 'center' },
-});
