b23d28bb32c53c313cef14638ad328bc7a31b078
korea7030 <korea7030@naver.com>
2020-11-23 11:32:01 +0900

    Hiding tab bar in specific screens

--- App.js
+++ App.js
@@ -3,48 +3,87 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import {  SafeAreaProvider, useSafeAreaInsets  } from 'react-native-safe-area-context';  // 화면 짤리는 부분 방지
+import { SafeAreaProvider, useSafeArea } from 'react-native-safe-area-context';
 
-function Demo() {
-  const insets = useSafeAreaInsets();
-
-  return (
-    // 위에 짤리는 부분 개선
-    <View
-      style={{
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-        flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'center'}}
-    >
-      <Text>This is top Text</Text>
-      <Text>This is bottom Text</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function Feed() {
+  return (
+    <View>
+      <Text>Feed</Text>
+    </View>
+  );
+}
+
+
+function Notifications() {
+  return (
+    <View>
+      <Text>Notifications</Text>
+    </View>
+  );
+}
+
+function Profile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+}
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Notifications" component={Notifications} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
-    // 위에 짤리는 부분 개선
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" headerMode="nome">
-          <Stack.Screen name="Home">
-            {() => {
-              <Tab.Navigator
-                initialRouteName="Analitics"
-                tabBar={() => null}
-              >
-                <Tab.Screen name="Analitics" component={Demo} />
-                <Tab.Screen name="Profile" component={Demo} />
-              </Tab.Navigator>
-            }}
-          </Stack.Screen>
-          <Stack.Screen name="Settings" component={Demo} />
+        {/* <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeStack} />
+          <Tab.Screen name="Feed" component={Feed} />
+          <Tab.Screen name="Notifications" component={Notifications} />
+        </Tab.Navigator> */}
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeTabs} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Settings" component={Settings} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
