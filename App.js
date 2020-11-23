2f65e8872d569e46da2d0284e5b54160794326e7
korea7030 <korea7030@naver.com>
2020-11-23 12:05:19 +0900

    Different status bar configuration based on route

--- App.js
+++ App.js
@@ -1,91 +1,61 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Text, StatusBar, Button, StyleSheet } from 'react-native';
+import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider, useSafeArea } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SafeAreaView from 'react-native-safe-area-view';
 
+// tab이나 drawer를 쓸 경우, 화면이 바꼈을때의 포커스 여부를 통해 화면을 표시 가능
+function FocusAwareStatusBar(props) {
+  const isFocused = useIsFocused();
 
-const Stack = createStackNavigator();
+  return isFocused ? <StatusBar {...props} /> : null;
+}
+
+function Screen1({ navigation }) {
+  return (
+    <SafeAreaView style={[styles.ccontainer, { backgroundColor: '#6a51ae'}]}>
+      <FocusAwareStatusBar  barStyle="light-content" backgroundColor="#6a51ae" />
+      <Text style={{ color: '#fff' }}>Light Screen</Text>
+      <Button
+        title="Next screen"
+        onPress={() => navigation.navigate('Screen2')}
+        color='#fff'
+      />
+    </SafeAreaView>
+  );
+}
+
+function Screen2({ navigation }) {
+  return (
+    <SafeAreaView style={[styles.container, { backgroundColor: '#ecf0f1'}]}>
+      <FocusAwareStatusBar  barStyle="dark-content" backgroundColor="#ecf0f1" />
+      <Text>Dark Screen</Text>
+      <Button
+        title="Next screen"
+        onPress={() => navigation.navigate('Screen1')}
+      />
+    </SafeAreaView>
+  )
+}
+
+// const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
-function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-}
-
-function Feed() {
-  return (
-    <View>
-      <Text>Feed</Text>
-    </View>
-  );
-}
-
-
-function Notifications() {
-  return (
-    <View>
-      <Text>Notifications</Text>
-    </View>
-  );
-}
-
-function Profile() {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  );
-}
-
-function Settings() {
-  return (
-    <View>
-      <Text>Settings</Text>
-    </View>
-  );
-}
-
-
-function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Settings" component={Settings} />
-    </Stack.Navigator>
-  );
-}
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Notifications" component={Notifications} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        {/* <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeStack} />
-          <Tab.Screen name="Feed" component={Feed} />
-          <Tab.Screen name="Notifications" component={Notifications} />
-        </Tab.Navigator> */}
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeTabs} />
-          <Stack.Screen name="Profile" component={Profile} />
-          <Stack.Screen name="Settings" component={Settings} />
-        </Stack.Navigator>
+        <Tab.Navigator headerMode="none">
+          <Tab.Screen name="Screen1" component={Screen1} />
+          <Tab.Screen name="Screen2" component={Screen2} />
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex:1, justifyContent: 'center', alignItems: 'center' },
+});
